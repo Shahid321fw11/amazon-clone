0a093ec4b01816b09e6e8e342c4059918e1c7f8c
Shahid321fw11 <shahid_fw11_321@masai.school>
2022-01-21 18:20:50 +0530

    Navbar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD1P97r2jnv7mESaSzm2IEAgl5euLFke8Q",
+    authDomain: "clone-b836d.firebaseapp.com",
+    projectId: "clone-b836d",
+    storageBucket: "clone-b836d.appspot.com",
+    messagingSenderId: "388493112049",
+    appId: "1:388493112049:web:08c8fe7679ea0a6f90af50",
+    measurementId: "G-STWW5SNTY1"
+  };
