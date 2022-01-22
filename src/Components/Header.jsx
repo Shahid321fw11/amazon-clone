daf1e24b0a6bdf4328f8933074bab8b65f7a9515
Shahid321fw11 <shahid_fw11_321@masai.school>
2022-01-22 14:47:59 +0530

    some feature added into home page

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import '../Components/Header.css';
 import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+
 
 function Header(){
     return(
+        // top header , its divided into 3 part
         <div className="header">
+            {/* 1st part */}
             <img className="header_logo" src="https://pngimg.com/uploads/amazon/amazon_PNG11.png"></img>
 
+            {/* 2nd part */}
             <div className="header_search">
                 <input type="text" className="header_searchInput"/>
                 <SearchIcon className="header_searchIcon"/>
-                
             </div>
 
+            {/* 3rd part */}
             <div className="header_nav">
                 <div className="header_option">
                     <span className="header_optionLineOne">Hello </span>
@@ -25,6 +30,10 @@ function Header(){
                 <div className="header_option">
                     <span className="header_optionLineOne">Your </span>
                     <span className="header_optionLineTwo">Prime</span>
+                </div>
+                <div className="header_optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header_optionLineTwo header_basketCount">0</span>
                 </div>
 
             </div>
